f27be20844aa04c9e55f9e8eea02363ba29b92d2
Ashley Rediger <ashleyrediger@users.campus>
2022-01-24 15:34:43 -0600

    test

---
+++ Alarm501/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarm501
+{
+    public partial class Alarm501 : Form
+    {
+        public Alarm501()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
